7a9d5e536d62a07fd5fe20686d55454f21bb34f3
John Karahalis <john@johnkarahalis.com>
2025-12-31 19:49:49 -0500

    Add "Tag: " before titles, headings on tag pages

--- custom.js
+++ custom.js
@@ -16,3 +16,12 @@ script.setAttribute("defer", true);
 script.setAttribute("src", "https://cloud.umami.is/script.js");
 script.setAttribute("data-website-id", "e28973f9-e5ca-4d45-8162-79a1c1c4cd9f");
 document.getElementsByTagName("head")[0].appendChild(script);
+
+// Add "Tag: " before the title and content heading on a category page.
+if (window.location.pathname.includes("/tag:")) {
+  const title = document.querySelector("title");
+  title.textContent = `Tag: ${title.textContent}`;
+
+  const contentHeading = document.querySelector("#wrapper h1");
+  contentHeading.textContent = `Tag: ${contentHeading.textContent}`;
+}
